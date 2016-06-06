16b4df962c71736ba8f94d2d66a59c8b6cc96b2b
Rev Herr <revuniversal@gmail.com>
2016-06-05 21:52:45 -0500

    stopped ignoring .idea/

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import { Card, CardTitle, CardText } from 'material-ui/Card';
 
 export const App = ({
     title,
     subtitle
 }) => (
-    <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <div>
-            <AppBar title={title} />
-            <Card>
-                <CardTitle
-                    title={title} 
-                    subtitle={subtitle}/>
-                
-                <CardText>
-                    <p>This is where content would go</p>
-                </CardText>
-            </Card>
-        </div>
-    </MuiThemeProvider>
+    <div>
+        <h1>{title}</h1>
+    </div>
 );
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import { App } from './app/app'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
+
 ReactDOM.render(
-    <App 
-        title="Just React" 
-        subtitle="and material ui"/>, 
+    <App title="Just React" />
     document.getElementById('AppHost'));
